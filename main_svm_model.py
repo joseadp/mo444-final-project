784791ee975d0c953d44252167da964354e41542
jose.pereira <jose.pereira@sofist.com.br>
2015-12-02 15:29:37 -0200

    SVM classifier [by Aditya]

---
+++ main_svm_model.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 02 01:39:48 2015
+
+@author: Aditya
+"""
+
+from svm_model import SvmModel
+from constants import *
+
+def apply_svm_model(filename):
+    sm = SvmModel(filename, estimators=500)
+
+    #rf.split_data()
+    sm.start_data()
+    sm.train_svm_model()
+    sm.test_svm_model()
+
+    sm.switch_folds()
+    sm.train_svm_model()
+    sm.test_svm_model()
+
+
+apply_svm_model(features_directory + filename)
